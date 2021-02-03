9d3c2d44e0092c8b023d544e3768da5152b6d6a1
Jacob Janak <jakeathotmail@gmail.com>
2021-02-02 23:29:40 -0500

    progress

--- blockchain.js
+++ blockchain.js
@@ -1,9 +1,9 @@
 const sha256 = require('js-sha256');
 
-const blockchain = [];
+// const blockchain = [];
 
 class Transcation {
-    constructor(from, to, amount) {
+    constructor(blockchain, from, to, amount) {
 
         // transaction details
         this.from = from;
@@ -18,16 +18,34 @@ class Transcation {
         const stringToHash = this.time + this.from + this.amount + this.to;
         if (blockchain.length > 0) {
             const prevHash = blockchain[blockchain.length - 1].hash;
-            this.hash = sha256.hmac(prevHash, stringToHash);
+            // this.hash = sha256.hmac(prevHash, stringToHash);
+            this.hash = sha256.hmac("", prevHash);
         } else {
             this.hash = sha256.hmac('Hello World!', stringToHash);
+            // this.hash = sha256.hmac("", "Hello World!");
         }
+
+        // get 5 more hashes for a total of 6
+        let newHash = this.hash;
+        this.hash += sha256.hmac(this.hash, this.from);
+        this.hash += sha256.hmac(this.hash, this.to);
+        this.hash += sha256.hmac(this.hash, String(this.amount));
+        this.hash += sha256.hmac(this.hash, String(this.time));
+        this.hash += sha256.hmac(this.hash, "temp");
+
+        // create random picture
+        this.picHexCodes = [];
+        for (let i = 0; i < 8; i++) {
+            const row = [];
+            for (let j = 0; j < 8; j++) {
+                const startIndex = 6 * (i * 8 + j);
+                row.push(this.hash.substring(startIndex, startIndex + 6))
+            }
+            this.picHexCodes.push(row)
+        }
+
+        // console.log(this.picHexCodes)
     }
 }
 
-blockchain.push(new Transcation('santa', 'me', 800))
-blockchain.push(new Transcation('mom', 'me', 100))
-blockchain.push(new Transcation('DJ', 'me', 420))
-blockchain.push(new Transcation('me', 'amazon', 1000))
-
-console.log(blockchain)
+module.exports = Transcation;
--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 require('dotenv').config()
 const express = require('express');
 const path = require('path');
-const blockchain = require('./blockchain');
+const blockchain = require('./blockchain.js');
 
 const app = express();
 const PORT = 8000;
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './index.html'))
+})
+
+app.get('/api/transactions', (req, res) => {
+
+    // dummy data
+    const transactions = [];
+    transactions.push(new blockchain(transactions, '' + Math.random(), '' + Math.random(), Math.random()))
+    transactions.push(new blockchain(transactions, 'mom', 'me', 100))
+    transactions.push(new blockchain(transactions, 'DJ', 'me', 420))
+    transactions.push(new blockchain(transactions, 'me', 'amazon', 1000))
+
+    res.json(transactions)
 })
 
 app.listen(PORT, () => console.log('Server running at localhost:' + PORT))
